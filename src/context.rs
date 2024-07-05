a54228c3452031bd07a296760b0a5ca75ffbb953
Piotr Kołaczkowski <pkolaczk@gmail.com>
2024-07-05 16:53:20 +0200

    Use Option for datacenter to keep backwards compatibility
    Adding non-optional datacenter field to the config
    made it impossible to load old reports.
    
    (cherry picked from commit 84988ee1ba9a28c684f3307e99c23d4951facb7b)

--- src/context.rs
+++ src/context.rs
@@ -64,11 +64,12 @@ fn ssl_context(conf: &&ConnectionConf) -> Result<Option<SslContext>, CassError> 
 /// Configures connection to Cassandra.
 pub async fn connect(conf: &ConnectionConf) -> Result<Context, CassError> {
     let mut policy_builder = DefaultPolicy::builder().token_aware(true);
-    let dc = &conf.datacenter;
-    if !dc.is_empty() {
+    let mut datacenter: String = "".to_string();
+    if let Some(dc) = &conf.datacenter {
         policy_builder = policy_builder
             .prefer_datacenter(dc.to_owned())
             .permit_dc_failover(true);
+        datacenter = dc.clone();
     }
     let profile = ExecutionProfile::builder()
         .consistency(conf.consistency.scylla_consistency())
@@ -88,7 +89,7 @@ pub async fn connect(conf: &ConnectionConf) -> Result<Context, CassError> {
     Ok(Context::new(
         Some(scylla_session),
         conf.page_size.get() as u64,
-        dc.to_string(),
+        datacenter,
         conf.retry_number,
         conf.retry_interval,
     ))
